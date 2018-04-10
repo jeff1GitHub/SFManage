c817c49e5e01b2fa21174451cc79791d23771ec0
jeff <jeff@orientalgame99.com>
2018-04-10 07:20:42 -0400

    增加数据库相关工具

--- src/main/java/com/sf/manage/ManageApplication.java
+++ src/main/java/com/sf/manage/ManageApplication.java
@@ -1,10 +1,12 @@
 package com.sf.manage;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
+@MapperScan("com.sf.manage.mapper")
 @SpringBootApplication
 public class ManageApplication extends SpringBootServletInitializer {
 
